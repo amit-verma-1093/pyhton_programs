dc1e691a59f5b1d2a1ef7a96a13dff0aaf144fc6
amit-verma-1093 <amitverma10931@gmail.com>
2026-09-16 09:27:38 +0530

    Add files via upload

---
+++ find_unique.py
@@ -0,0 +1,29 @@
+# s=input("enter the string: ")
+# count=0
+# for i in s.lower():
+#     if i in vowels:
+#         count+=1
+# print(count)
+
+# for i in range(1,6):
+#     print ((10**i//9)**2)
+# for i in range(1,6):
+#     print((10**i//9)*i)
+
+# def count_substring(string, sub_string):
+#     c=0
+#     for i in range (0,len(string)):
+#         slice=string[i:i+len(sub_string)]
+#         if slice==sub_string:
+#             c+=1
+#     return c
+# if __name__ == '__main__':
+#     string = input().strip()
+#     sub_string = input().strip()
+    
+#     count = count_substring(string, sub_string)
+#     print(count)
+
+l=[2,4,5,6,7,6]
+for i in range(-1,0,-1):
+    print(i)
