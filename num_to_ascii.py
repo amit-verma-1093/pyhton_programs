2f86cc50e84159c2a565ce810391e295de2b574d
amit-verma-1093 <amitverma10931@gmail.com>
2026-09-19 05:51:28 +0530

    Add files via upload

---
+++ num_to_ascii.py
@@ -0,0 +1,35 @@
+#conversion from number to ascii
+num=int(input())
+# lst=[]
+# while num!=0:
+#     val=num%100
+#     lst.append(val)
+#     num=num//100
+# st=""
+# lst=lst[::-1]
+# for i in lst:
+#     st+=chr(i)
+# print(st)
+
+## revese a int using list
+
+# lst=[]
+# while num!=0:
+#     val=num%10
+#     lst.append(val)
+#     num=num//10
+# for i in lst:
+#     print(i,end="")
+  
+#reverse a int 
+# rev=0
+# while num!=0:
+#     digit=num%10
+#     rev=rev*10+digit
+#     num=num//10
+# print(rev)
+
+# reverse using str()
+
+# n=str(num)
+# print(n[::-1])
