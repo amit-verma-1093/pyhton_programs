49d31b518a03426dd8c77d4ee7ceadd93daac79a
amit-verma-1093 <amitverma10931@gmail.com>
2026-09-23 19:55:04 +0530

    Add files via upload

---
+++ que3.py
@@ -0,0 +1,18 @@
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         if i in [2,4]:
+#             print(i,end=" ")
+    
+#         else:
+#             print(j,end=" ")
+    
+#     print()
+# 1
+# 22
+# 123
+# 4444
+# 12345
+n=int(input())
+# for i in range(len()
+s=str(n)
+print(len(s))
